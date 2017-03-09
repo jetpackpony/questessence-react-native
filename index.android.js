0dee451c3b70c33daab1e91af3d1a6775824de5d
jetpackpony <jetpackpony@gmail.com>
2017-03-09 18:17:09 +0300

    Add initial quest list layout

--- index.android.js
+++ index.android.js
@@ -9,12 +9,52 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image
 } from 'react-native';
-import { Container, Header, Title, Content, Body, Left, Right } from 'native-base';
+import {
+  Container, Header, Title,
+  Content, Body, Left, Right,
+  Card, CardItem,
+  H1
+} from 'native-base';
+
+const imgs = [{
+  img: 'https://kudago.com/media/images/news/d3/2e/d32ecc1b3e4aa49f49c5f220a8dc88b8.jpg',
+  title: 'Петропавловская Крепость и Окрестности'
+}, {
+  img: 'https://d3hdm9bjz45yxn.cloudfront.net/userfiles/excursiongalleries/pictureshd/345/4129.jpeg',
+  title: 'Русский Музей'
+}, {
+  img: 'http://estelhautecouture.ru/assets/i/upload/20150401/dfa173b463af881eade974a743ac8929.jpg',
+  title: 'Легенды Петергофа'
+}];
 
 export default class QuestEssence extends Component {
   render() {
+    const cards = imgs.map((quest, i) => {
+      return (
+        <Card key={i}>
+          <CardItem cardBody>
+            <View style={styles.cardImageContainer}>
+              <Image
+                resizeMode='cover'
+                style={styles.cardImage}
+                source={{ uri: quest.img }}>
+                <View style={{ flex: 1 }}></View>
+                <View style={{ flex: 1 }}>
+                  <View style={styles.textUnderlay}></View>
+                  <View style={styles.textContainer}>
+                    <Text style={styles.cardHeader}>{quest.title}</Text>
+                    <Text style={styles.cardDescription}>Это краткое описание всех приключений, которые ждут вас в этом квесте. Всё так классно и здорово, что хочется блевать радугой, вокруг бегают единороги и все такие счастливые, будто бы обожрались ЛСД. Отличный способ провести время с близкими и друзьями!</Text>
+                  </View>
+                </View>
+              </Image>
+            </View>
+          </CardItem>
+        </Card>
+      );
+    });
     return (
       <Container>
         <Header>
@@ -23,12 +63,44 @@ export default class QuestEssence extends Component {
           </Body>
         </Header>
         <Content padder>
-          <Text>Content here</Text>
+          {cards}
         </Content>
       </Container>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  cardImageContainer: {
+    flex: 1,
+    height: 300
+  },
+  cardImage: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0
+  },
+  textUnderlay: {
+    flex: 1,
+    backgroundColor: 'black',
+    opacity: 0.6
+  },
+  textContainer: {
+    position: 'absolute',
+    top: 0,
+    margin: 10,
+    overflow: 'hidden'
+  },
+  cardHeader: {
+    fontSize: 18,
+    color: 'white'
+  },
+  cardDescription: {
+    color: 'white'
+  }
+})
 
 
 AppRegistry.registerComponent('QuestEssence', () => QuestEssence);
