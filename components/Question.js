268d5fa338a55aa0be35efe3ffdc9e524c73118d
jetpackpony <jetpackpony@gmail.com>
2017-03-10 18:42:20 +0300

    Added basic answer checking logic

--- components/Question.js
+++ components/Question.js
@@ -5,8 +5,60 @@ import { Container, Content, Footer,
 
 import QuestionMedia from '../components/QuestionMedia';
 
+const QUESTION_STATE = {
+  UNANSWERED: 0, CORRECT: 1, INCORRECT: 2
+};
+
 export default class Question extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      answer: undefined,
+      showAnswer: false,
+      questionState: QUESTION_STATE.UNANSWERED
+    };
+
+    this.submitAnswer = this.submitAnswer.bind(this);
+  }
+
+  submitAnswer() {
+    if (this.state.answer === this.props.question.answer) {
+      this.setState({ questionState: QUESTION_STATE.CORRECT });
+    } else {
+      this.setState({ questionState: QUESTION_STATE.INCORRECT });
+    }
+  }
+
   render() {
+    const correct = this.state.questionState === QUESTION_STATE.CORRECT;
+    const incorrect = this.state.questionState === QUESTION_STATE.INCORRECT;
+
+    let correctBlock = null;
+    if (correct) {
+      correctBlock = (
+        <View>
+          <Text>Правильно!</Text>
+          <Text>{this.props.question.answerDesc}</Text>
+        </View>
+      );
+    }
+    if (incorrect) {
+      correctBlock = (
+        <View>
+          <Text>Неправильно!</Text>
+          <Text>Попробуйте ещё раз</Text>
+        </View>
+      );
+    }
+    if (this.state.showAnswer) {
+      correctBlock = (
+        <View>
+          <Text>Правильный ответ: {this.props.question.answer}</Text>
+          <Text>{this.props.question.answerDesc}</Text>
+        </View>
+      );
+    }
+    const showForm = this.state.questionState !== QUESTION_STATE.CORRECT && !this.state.showAnswer;
     return (
       <View>
         <View style={styles.mediaContainer}>
@@ -21,15 +73,38 @@ export default class Question extends Component {
           <Text style={styles.questionText}>
             {this.props.question.questionText}
           </Text>
-          <Form>
-            <Item floatingLabel >
-              <Label>Ответ:</Label>
-              <Input />
-            </Item>
-          </Form>
-          <Button block style={{ margin: 10 }}>
-            <Text>Готово</Text>
-          </Button>
+
+          {correctBlock}
+
+          {((showForm) ?
+            <View>
+              <Form>
+                <Item floatingLabel >
+                  <Label>Ответ:</Label>
+                  <Input
+                    onChangeText={(answer) => this.setState({ answer })}
+                  />
+                </Item>
+              </Form>
+              <Button
+                block
+                style={{ margin: 10 }}
+                onPress={this.submitAnswer}
+              >
+                <Text>Готово</Text>
+              </Button>
+            </View>
+            : null
+          )}
+
+          {(correct || this.state.showAnswer)
+              ? <Button><Text>Дальше</Text></Button>
+              : null
+          }
+          {(incorrect && !this.state.showAnswer)
+              ? <Button onPress={() => this.setState({ showAnswer: true })}><Text>Узнать ответ</Text></Button>
+              : null
+          }
         </View>
       </View>
     );
