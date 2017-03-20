17fdf120774ac881b4761f5ee97bbaed9181c3e3
jetpackpony <jetpackpony@gmail.com>
2017-03-20 14:12:27 +0300

    Add reducer for START_QUEST and ANSWER_QUESTION actions

--- __tests__/QuestessenceReducer.js
+++ __tests__/QuestessenceReducer.js
@@ -1,13 +1,64 @@
-import reducer from '../reducers/QuestessenceReducer';
-import { startQuest } from '../actions/Actions.js';
+import {
+  QuestessenceReducer,
+  initialState
+} from '../reducers/QuestessenceReducer';
+import { startQuest, answerQuestion } from '../actions/Actions.js';
 import { QuestStates, QuestionStates } from '../actions/Actions';
 
-it('it starts the quest', () => {
-  let { questProgress } = reducer(undefined, startQuest(0));
-  expect(questProgress.quest0).toBe({
+const addDummyQuest = (inState) => {
+  let state = { ...inState };
+  state.questData.quests.push({
+    id: 0,
+    title: 'Test',
+    cover: 'http://...',
+    desc: 'Description'
+  });
+  state.questData.questions.quest0 = [{
+    media: {
+      type: 'image',
+      uri: 'https://'
+    },
+    desc: 'На этом месте будет описание ',
+    questionText: 'Сколько планет в солнечной системе?',
+    answer: '8',
+    answerDesc: 'Потому что Плутон больше не планета'
+  },{
+    media: {
+      type: 'image',
+      uri: 'https://'
+    },
+    desc: 'На этом месте будет описание вопроса',
+    questionText: 'Сколько часов в сутках?',
+    answer: '24',
+    answerDesc: 'Хотелось бы больше, конечно'
+  }];
+  state.questProgress.quest0 = {
     questState: QuestStates.IN_PROGRESS,
     currentQuestion: 0,
     currentAnswer: '',
     currentQuestionState: QuestionStates.UNANSWERED
-  });
+  }
+  return state;
+};
+
+it('starts a quest', () => {
+  expect(
+    QuestessenceReducer(initialState, startQuest(0))
+  ).toMatchSnapshot();
+});
+
+it('sets the question state if the answer is correct', () => {
+  let state = addDummyQuest(initialState);
+  state.questData.questions.quest0[0].answer = '8';
+  expect(
+    QuestessenceReducer(state, answerQuestion(0, 0, '8'))
+  ).toMatchSnapshot();
+});
+
+it('sets the question state if the answer is incorrect', () => {
+  let state = addDummyQuest(initialState);
+  state.questData.questions.quest0[1].answer = '24';
+  expect(
+    QuestessenceReducer(state, answerQuestion(0, 1, '15'))
+  ).toMatchSnapshot();
 });
--- reducers/QuestessenceReducer.js
+++ reducers/QuestessenceReducer.js
@@ -1,6 +1,6 @@
 import { QuestStates, QuestionStates } from '../actions/Actions';
 
-const initialState = {
+export const initialState = {
   questData: {
     quests: [],
     questions: {}
@@ -8,8 +8,11 @@ const initialState = {
   questProgress: {}
 };
 
-export default function QuestessenceReducer(state = initialState, action) {
-  /*
+const isAnswerCorrect = (question, answer) => {
+  return question.answer === answer;
+};
+
+export function QuestessenceReducer(state = initialState, action) {
   switch (action.type) {
     case 'START_QUEST':
       return {
@@ -17,12 +20,27 @@ export default function QuestessenceReducer(state = initialState, action) {
         questProgress: {
           ...state.questProgress,
           [`quest${action.questId}`]: {
-            ...state.questProgress[`quest${action.questId}`],
-
+            questState: QuestStates.IN_PROGRESS,
+            currentQuestion: 0,
+            currentAnswer: '',
+            currentQuestionState: QuestionStates.UNANSWERED
           }
         }
-      }
+      };
+    case 'ANSWER_QUESTION':
+      let question = state.questData.questions[`quest${action.questId}`][action.questionId];
+      return {
+        ...state,
+        questProgress: {
+          ...state.questProgress,
+          [`quest${action.questId}`]: {
+            ...state.questProgress[`quest${action.questId}`],
+            currentAnswer: action.answer,
+            currentQuestionState: isAnswerCorrect(question, action.answer) ? QuestionStates.CORRECT : QuestionStates.INCORRECT
+          }
+        }
+      };
+    default:
+      return state;
   }
-  */
-  return state;
 };
