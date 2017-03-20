d738df35d6abf2fca358ec9a79dcd91b125c8c5c
jetpackpony <jetpackpony@gmail.com>
2017-03-20 18:15:43 +0300

    Make addDummyQuest into a pure function

--- __tests__/QuestessenceReducer.js
+++ __tests__/QuestessenceReducer.js
@@ -2,43 +2,58 @@ import {
   QuestessenceReducer,
   initialState
 } from '../reducers/QuestessenceReducer';
-import { startQuest, answerQuestion } from '../actions/Actions.js';
+import {
+  startQuest,
+  answerQuestion,
+  showCorrectAnswer,
+  goToNextQuestion
+} from '../actions/Actions.js';
 import { QuestStates, QuestionStates } from '../actions/Actions';
 
 const addDummyQuest = (inState) => {
-  let state = { ...inState };
-  state.questData.quests.push({
-    id: 0,
-    title: 'Test',
-    cover: 'http://...',
-    desc: 'Description'
-  });
-  state.questData.questions.quest0 = [{
-    media: {
-      type: 'image',
-      uri: 'https://'
+  return {
+    ...inState,
+    questData: {
+      ...inState.questData,
+      quests: inState.questData.quests.concat({
+        id: 0,
+        title: 'Test',
+        cover: 'http://...',
+        desc: 'Description'
+      }),
+      questions: {
+        ...inState.questData.questions,
+        quest0: [{
+          media: {
+            type: 'image',
+            uri: 'https://'
+          },
+          desc: 'На этом месте будет описание ',
+          questionText: 'Сколько планет в солнечной системе?',
+          answer: '8',
+          answerDesc: 'Потому что Плутон больше не планета'
+        },{
+          media: {
+            type: 'image',
+            uri: 'https://'
+          },
+          desc: 'На этом месте будет описание вопроса',
+          questionText: 'Сколько часов в сутках?',
+          answer: '24',
+          answerDesc: 'Хотелось бы больше, конечно'
+        }]
+      }
     },
-    desc: 'На этом месте будет описание ',
-    questionText: 'Сколько планет в солнечной системе?',
-    answer: '8',
-    answerDesc: 'Потому что Плутон больше не планета'
-  },{
-    media: {
-      type: 'image',
-      uri: 'https://'
-    },
-    desc: 'На этом месте будет описание вопроса',
-    questionText: 'Сколько часов в сутках?',
-    answer: '24',
-    answerDesc: 'Хотелось бы больше, конечно'
-  }];
-  state.questProgress.quest0 = {
-    questState: QuestStates.IN_PROGRESS,
-    currentQuestion: 0,
-    currentAnswer: '',
-    currentQuestionState: QuestionStates.UNANSWERED
-  }
-  return state;
+    questProgress: {
+      ...inState.questProgress,
+      quest0: {
+        questState: QuestStates.IN_PROGRESS,
+        currentQuestion: 0,
+        currentAnswer: '',
+        currentQuestionState: QuestionStates.UNANSWERED
+      }
+    }
+  };
 };
 
 it('starts a quest', () => {
