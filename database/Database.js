43220c13d2bd52b01163231b41582bb241369980
jetpackpony <jetpackpony@gmail.com>
2017-03-30 21:05:54 +0300

    Add a progress sync function

--- database/Database.js
+++ database/Database.js
@@ -10,7 +10,7 @@ const listenToQuests = (callback) => {
   });
 };
 
-const downloadQuestions = (questId, callback) => {
+const downloadQuestions = (questId) => {
   return firebase.database()
     .ref('questions/byId')
     .orderByChild('quest')
@@ -19,4 +19,27 @@ const downloadQuestions = (questId, callback) => {
     .then((snapshot) => snapshot.val());
 };
 
-export default { listenToQuests, downloadQuestions };
+const syncQuestsProgress = (uid, progress) => {
+  if (!uid) return Promise.resolve(progress);
+
+  return firebase.database()
+    .ref(`usersProgress/byId/${uid}`)
+    .once('value')
+    .then((snapshot) => snapshot.val())
+    .then((value) => {
+      if (!value || value.timestamp <= progress.timestamp) {
+        return uploadUserProgress(uid, progress);
+      } else {
+        return value;
+      }
+    });
+};
+
+const uploadUserProgress = (uid, progress) => {
+  return firebase.database()
+    .ref(`usersProgress/byId/${uid}`)
+    .set(progress)
+    .then(() => progress);
+};
+
+export default { listenToQuests, downloadQuestions, syncQuestsProgress };
