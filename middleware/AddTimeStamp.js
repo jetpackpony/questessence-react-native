1b243c8099651f334a0c1c513c683a953fe60195
jetpackpony <jetpackpony@gmail.com>
2017-03-31 19:21:02 +0300

    Removed console logs from middleware

--- middleware/AddTimeStamp.js
+++ middleware/AddTimeStamp.js
@@ -1,13 +1,11 @@
 
 export default ({ getState }) => (next) => (action) => {
   if (action.updatingProgress) {
-    console.log("Updating Progress");
     return next({
       ...action,
       timestamp: Date.now()
     });
   } else {
     return next(action);
-    console.log("NOT Updating Progress");
   }
 };
