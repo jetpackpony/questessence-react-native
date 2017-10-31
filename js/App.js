7abc0a152351b53c3180722a62e67ac98bfe0e66
jetpackpony <jetpackpony@gmail.com>
2017-10-31 20:51:43 +0300

    Refactor App.js
      * extract initializing store into a separate file
      * extract creating a navigator into a separate file
      * use functional component instead of class

--- js/App.js
+++ js/App.js
@@ -1,68 +1,13 @@
-import React, { Component } from 'react';
-import { AppRegistry, AsyncStorage } from 'react-native';
-import { StackNavigator } from 'react-navigation';
+import React from 'react';
+import { AppRegistry } from 'react-native';
 import { Provider } from 'react-redux'
-import { createStore, compose, applyMiddleware } from 'redux'
-import { persistStore, autoRehydrate } from 'redux-persist';
-import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+import initStore from './initStore';
+import AppNavigator from './AppNavigator';
 
-import HomeScreen from './screens/HomeScreen';
-import QuestScreen from './screens/QuestScreen';
-import QuestQuestionsScreen from './screens/QuestQuestionsScreen';
-import QuestessenceReducer from './reducers/QuestessenceReducer.js';
-import { updateQuestList, syncProgress } from './actions/Actions';
-import { listenToQuests } from './database/Database';
-import addTimeStamp from './middleware/AddTimeStamp';
-import syncStorageWithFirebase from './middleware/SyncStorageWithFirebase';
-
-import { dontHydrateKeys } from './reducers/QuestessenceReducer';
-
-import { PRIMARY_COLOR, HEADER_TEXT_COLOR } from './Colors';
-
-const QuestEssenceNavigator = StackNavigator({
-  Home: { screen: HomeScreen },
-  Quest: { screen: QuestScreen },
-  QuestQuestions: { screen: QuestQuestionsScreen },
-} , {
-  navigationOptions: {
-    headerStyle: {
-      backgroundColor: PRIMARY_COLOR
-    },
-    headerTintColor: HEADER_TEXT_COLOR
-  }
-});
-
-const store = createStore(
-  QuestessenceReducer,
-  compose(
-    autoRehydrate({ log: true }),
-    applyMiddleware(
-      thunk, addTimeStamp,
-      /* These middleware have to be at the end */
-      syncStorageWithFirebase, logger
-    )
-  )
+const QuestEssence = () => (
+  <Provider store={initStore()}>
+    <AppNavigator />
+  </Provider>
 );
-persistStore(store, {
-  storage: AsyncStorage,
-  blacklist: dontHydrateKeys
-}, () => {
-  // On rehydration complete
-  listenToQuests((quests) => {
-    store.dispatch(updateQuestList(quests))
-  });
-  store.dispatch(syncProgress());
-});
-
-class QuestEssence extends React.Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <QuestEssenceNavigator />
-      </Provider>
-    );
-  }
-}
 
 AppRegistry.registerComponent('QuestEssence', () => QuestEssence);
---
+++ js/initStore.js
@@ -0,0 +1,39 @@
+import { AsyncStorage } from 'react-native';
+import { createStore, compose, applyMiddleware } from 'redux'
+import { persistStore, autoRehydrate } from 'redux-persist';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import addTimeStamp from './middleware/AddTimeStamp';
+import QuestessenceReducer from './reducers/QuestessenceReducer.js';
+import { updateQuestList, syncProgress } from './actions/Actions';
+import { listenToQuests } from './database/Database';
+import syncStorageWithFirebase from './middleware/SyncStorageWithFirebase';
+import { dontHydrateKeys } from './reducers/QuestessenceReducer';
+
+export default () => {
+  const store = createStore(
+    QuestessenceReducer,
+    compose(
+      autoRehydrate({ log: true }),
+      applyMiddleware(
+        thunk, addTimeStamp,
+        /* These middleware have to be at the end */
+        syncStorageWithFirebase, logger
+      )
+    )
+  );
+
+  // Setup rehydration
+  persistStore(store, {
+    storage: AsyncStorage,
+    blacklist: dontHydrateKeys
+  }, () => {
+    // On rehydration complete
+    listenToQuests((quests) => {
+      store.dispatch(updateQuestList(quests))
+    });
+    store.dispatch(syncProgress());
+  });
+
+  return store;
+};
