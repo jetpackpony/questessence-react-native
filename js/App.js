b8a6ed518499dfbce43b29a7674f0c6c3f17596d
jetpackpony <jetpackpony@gmail.com>
2019-09-27 18:04:56 +0300

    Add app's status bar color

--- js/App.js
+++ js/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, StatusBar } from 'react-native';
 import { Provider } from 'react-redux';
 import initStore from './initStore';
 import { name as appName } from '../app.json';
 import { makeNavigationAppContainer } from './navigation';
 import useDrawer from './useDrawer';
 import DrawerContent from './components/DrawerContent';
+import { STATUS_BAR_COLOR } from './Colors';
 
 const QuestEssence = () => {
   const { Drawer, openDrawer } = useDrawer(<DrawerContent />);
@@ -14,6 +15,7 @@ const QuestEssence = () => {
   return (
     <Provider store={initStore()}>
       <Drawer>
+        <StatusBar backgroundColor={STATUS_BAR_COLOR} barStyle="light-content" />
         <AppContainer />
       </Drawer>
     </Provider>
