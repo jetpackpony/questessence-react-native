91b560dc430b9a520e4bb9bca9b33980e32a821d
jetpackpony <jetpackpony@gmail.com>
2019-09-27 14:08:48 +0300

    Refactor FB button to use HOC

--- js/components/FBLoginButton.js
+++ js/components/FBLoginButton.js
@@ -1,52 +1,27 @@
 import React from 'react';
 import { View } from 'react-native';
-import { LoginManager } from 'react-native-fbsdk';
-import { connect } from 'react-redux';
-import { loginFacebook, logout, loginStart } from '../actions';
 import PrimaryButton from './PrimaryButton';
 import I18n from '../locales/i18n';
+import withLogin from '../withLogin';
 
-const mapStateToProps = (state) => {
-  return {
-    isLoggedIn: state.user.isLoggedIn
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    onLogin: (error, result) => {
-      dispatch(loginStart());
-      dispatch(loginFacebook(error, result));
-    },
-    onLogout: () => dispatch(logout())
-  };
-};
-
-const FBLoginButton = ({ isLoggedIn, onLogin, onLogout }) => {
+export const FBLoginButton = ({ isLoggedIn, onLogin, onLogout }) => {
   return (
     <View>
       {
         (isLoggedIn)
           ? (
-            <PrimaryButton onPress={
-              () => {
-                LoginManager.logOut();
-                onLogout();
-              }
-            }>{I18n.t("logout")}</PrimaryButton>
+            <PrimaryButton onPress={onLogout}>
+              {I18n.t("logout")}
+            </PrimaryButton>
           )
           : (
-            <PrimaryButton onPress={
-              () => LoginManager.logInWithPermissions(['email'])
-                .then(
-                  (result) => onLogin(null, result),
-                  (error) => onLogin(error)
-                )
-            }>{I18n.t("loginWithFacebook")}</PrimaryButton>
+            <PrimaryButton onPress={onLogin}>
+              {I18n.t("loginWithFacebook")}
+            </PrimaryButton>
           )
       }
     </View>
   );
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(FBLoginButton);
+export default withLogin(FBLoginButton);
