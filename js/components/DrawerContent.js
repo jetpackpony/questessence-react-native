5f5dda35c42410dc85b5cfad58a0c67a60b96357
jetpackpony <jetpackpony@gmail.com>
2019-09-27 13:22:42 +0300

    Add styling to Drawer

--- js/components/DrawerContent.js
+++ js/components/DrawerContent.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { PRIMARY_COLOR } from '../Colors';
+import { List, ListItem, Text } from 'native-base';
 
 const DrawerContent = () => {
   return (
-    <View style={{ flex: 1, backgroundColor: '#fff' }}>
-      <Text style={{ margin: 10, fontSize: 15, textAlign: 'left' }}>I'm on a boat!</Text>
-    </View>
+    <List>
+      <ListItem noIndent first style={styles.header}>
+        <Text style={styles.headerText}>Questessence</Text>
+      </ListItem>
+      <ListItem button onPress={() => console.log("PP")}>
+        <Text>Privacy Policy</Text>
+      </ListItem>
+      <ListItem button onPress={() => console.log("Logout")}>
+        <Text>Logout</Text>
+      </ListItem>
+    </List>
+
   );
 };
+
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: PRIMARY_COLOR,
+    height: 80
+  },
+  headerText: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold"
+  }
+});
 
 export default DrawerContent;
