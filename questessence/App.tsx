a12e025bbbe9a715410897518aa43bfa1780a2d8
jetpackpony <jetpackpony@gmail.com>
2023-12-07 12:12:24 +0300

    Add initial expo setup

---
+++ questessence/App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>OPEN UP APP.TSX TO START WORKING ON YOUR APP!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
